bc73278961e0f60a1b996534d674b9099d83a00a
Airu <rizkykirigaya@gmail.com>
2023-07-22 15:06:29 +0700

    init project

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const mongoUrl = process.env.DATABASE_URL;
+const port = process.env.PORT;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.log("Failed to connect to mongoDB");
+    console.log(error.message);
+  });
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log("Server is running");
+});
