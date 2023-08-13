2d6c1eaf7ebd545664bbfbbaf42139ed45b4375f
Airu <rizkykirigaya@gmail.com>
2023-08-13 17:43:20 +0700

    add field videoOwnerUsername in getVideo

--- controller/VideoController.js
+++ controller/VideoController.js
@@ -1,5 +1,5 @@
 const { Video } = require("../models/Video");
-
+const User = require("../models/User");
 // Get Video
 const getAllVideo = (req, res, next) => {
   Video.find()
@@ -19,24 +19,78 @@ const getAllVideo = (req, res, next) => {
     });
 };
 
-const getVideo = (req, res, next) => {
+const getVideo = async (req, res, next) => {
   const { id: videoId } = req.params;
+  try {
+    const videoResponse = await Video.findById(videoId);
 
-  Video.find({ _id: videoId })
-    .then((response) => {
-      res.status(200).json({
-        data: response,
-        message: "Sucessfully to load video list",
+    if (!videoResponse) {
+      return res.status(404).json({
+        data: null,
+        message: "Video not found",
         error: null,
       });
-    })
-    .catch((error) => {
-      res.json({
-        data: null,
-        message: "Failed to load data",
-        error: error,
+    }
+
+    try {
+      const userResponse = await User.findById(videoResponse.videoOwner);
+
+      if (!userResponse) {
+        const { _id, title, thumbnailUrl, videoOwner, videoUrl } =
+          videoResponse;
+        return res.status(200).json({
+          data: {
+            _id,
+            title,
+            thumbnailUrl,
+            videoOwner,
+            videoUrl,
+          },
+          message: "Successfully loaded video and user data",
+          error: null,
+        });
+      }
+
+      console.log("I must be here");
+      console.log(videoResponse);
+
+      const { _id, title, thumbnailUrl, videoOwner, videoUrl } = videoResponse;
+      return res.status(200).json({
+        data: {
+          _id,
+          title,
+          thumbnailUrl,
+          videoOwner,
+          videoUrl,
+          videoOwnerUsername: userResponse.name,
+        },
+        message: "Successfully loaded video and user data",
+        error: null,
       });
+    } catch (error) {
+      // Fail to load user, but sttill get the video
+      const { _id, title, thumbnailUrl, videoOwner, videoUrl } = videoResponse;
+      return res.status(200).json({
+        data: {
+          _id,
+          title,
+          thumbnailUrl,
+          videoOwner,
+          videoUrl,
+        },
+        message: "Successfully loaded video and user data",
+        error: null,
+      });
+    }
+  } catch (error) {
+    console.log(0);
+    console.log(error);
+    return res.status(500).json({
+      data: null,
+      message: "Failed to load data",
+      error: error,
     });
+  }
 };
 
 // Post Video
